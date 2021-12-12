09ad2631d6969f006408b0398e36371abbb817c5
charly2045 <charly2045@gmail.com>
2021-12-12 12:55:01 +0100

    Acabando taller final del curso

--- analisis.js
+++ analisis.js
@@ -1,26 +1,51 @@
+function esPar(numero){
+    // forma mas abreviada sin los if
+    return (numero % 2 === 0);
+}
+
+function calcularMediaAritmetica(lista) {
+    const sumaLista = lista.reduce(
+      function (valorAcumulado = 0, nuevoElemento) {
+        return valorAcumulado + nuevoElemento;
+      }
+    );
+  
+    const promedioLista = sumaLista / lista.length;
+  
+    return promedioLista;
+}
+
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length / 2);
+
+    if (esPar(lista.length)) {
+        const personaMitad1 = lista[mitad - 1];
+        const personaMitad2 = lista[mitad];
+
+        const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2]);
+
+        return mediana;
+    } else {
+        const personaMitad = lista[mitad];
+        return personaMitad;
+    }
+}
 const salariosSp = spain.map(function(elemento){
-    return spain.salary;
+    return elemento.salary;
 
 });
 
 const salariosSpOrdenada = salariosSp.sort(function(salaryA, salaryB){
     return salaryA - salaryB;
-})
+});
 
-function salariosMediana(lista) {
-    const mitad = parseInt(lista.length / 2);
+const medianaGeneralSpain = medianaSalarios(salariosSpOrdenada);
 
-    if (esPar(lista.length)) {
+const spliceStart = 
 
-    } else {
-        const personaMitad = lista[mitad];
-        return personaMitad;
-    }
+const salariosTop10 = salariosSpOrdenada.splice();
 
 
-}
+// Llamamos a la función salariosMediada con la lista de salarios ordenada como argumento
 
-function esPar(numero){
-    // forma mas abreviada sin los if
-    return (numero % 2 === 0);
-}
+console.log(medianaGeneralSpain, medianaTop10Spain));
